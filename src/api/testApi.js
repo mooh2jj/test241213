1a12155fc247490849ecf17ac3a63f1b8368d9e5
seonggon do <mooh2jj@naver.com>
2024-12-16 18:56:34 +0900

    console 로그 삽입

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -5,6 +5,7 @@ const awsEc2IpUrl = process.env.AWS_EC2_IP_URL;
 
 // 테스트 API 호출
 export const getTest = async () => {
+  console.log("awsEc2IpUrl: ", awsEc2IpUrl);
   const response = await axios.get(`${awsEc2IpUrl}/api/test/list`);
   return response;
 };
