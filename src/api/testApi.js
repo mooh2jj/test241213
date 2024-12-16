44775d52449f4eb5d04e262b1295119ddaa3691d
seonggon do <mooh2jj@naver.com>
2024-12-16 18:10:22 +0900

    env url 변경 추가

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// .env 파일에 있는 AWS_EC2_IP_ADDRESS 값을 가져오기
-const AWS_EC2_IP_ADDRESS = process.env.AWS_EC2_IP_ADDRESS;
+// .env 파일에 있는 AWS_EC2_IP_URL 값을 가져오기
+const awsEc2IpUrl = process.env.AWS_EC2_IP_URL;
 
 // 테스트 API 호출
 export const getTest = async () => {
-  const response = await axios.get(`${AWS_EC2_IP_ADDRESS}:8080/api/test/list`);
+  const response = await axios.get(`${awsEc2IpUrl}/api/test/list`);
   return response.data;
 };
