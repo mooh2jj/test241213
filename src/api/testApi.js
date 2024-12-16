6954ca3ad273db7bbbe37de06bb953e83e5165c8
seonggon do <mooh2jj@naver.com>
2024-12-16 18:59:39 +0900

    접두어 붙임

--- src/api/testApi.js
+++ src/api/testApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // .env 파일에 있는 AWS_EC2_IP_URL 값을 가져오기
-const awsEc2IpUrl = process.env.AWS_EC2_IP_URL;
+const awsEc2IpUrl = process.env.REACT_APP_AWS_EC2_IP_URL;
 
 // 테스트 API 호출
 export const getTest = async () => {
