2185f7bcd7fbb89a06aeb08a5127ae7d6ace773b
seonggon do <mooh2jj@naver.com>
2024-12-16 10:09:53 +0900

    api 연동 Home에서 처리

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Home = () => {
+  // test
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/test/list").then((res) => {
+      console.log(res);
+      setTests(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul style={{ listStyle: "none" }}>
+        {tests.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
